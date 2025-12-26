d15ab7ff4cda8dfd2ada671492833bb166baa235
nahee <kimnahee@signal-decode.com>
2025-12-26 10:59:22 +0900

    폴더 구조, sass 세팅

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts'
+  ],
+
+  css: [
+    resolve(__dirname, 'assets/styles/main.scss'),
+  ],
+})
